ba186ba791928cca9dd58099aa552df5c5c41955
Saffan Baig <mirzasaffan492@gmail.com>
2020-02-11 22:14:51 +0500

    Implement Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router'
 
 import { AuthModule } from './auth/auth.module';
 import { MainModule } from './main/main.module';
 
 import { AppComponent } from './app.component';
 
+const routes: Routes = [
+  {path:'', pathMatch: 'full', redirectTo: 'movies'}
+];
 @NgModule({
   declarations: [
     AppComponent
@@ -13,7 +17,11 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AuthModule,
-    MainModule
+    MainModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
